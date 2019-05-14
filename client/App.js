3284c4d4a09f69f8be2c74f0c11b097816f2a37b
Dominik Rodzim <dominik.rodzim95@gmail.com>
2019-05-14 22:03:01 +0200

    Add fixes

--- client/App.js
+++ client/App.js
@@ -17,6 +17,35 @@ class App extends Component {
 		this.state = {users: [], messages: [], text: '', name: ''};
 	}
 
+	renderUserForm() {
+	   return (<UserForm onUserSubmit={name => this.handleUserSubmit(name)} />)
+	}
+
+	componentDidMount() {
+	  socket.on('message', message => this.messageReceive(message));
+	  socket.on('update', ({users}) => this.chatUpdate(users));
+	}
+
+	messageReceive(message) {
+	  const messages = [message, ...this.state.messages];
+	  this.setState({messages});
+	}
+
+	chatUpdate(users) {
+	  this.setState({users});
+	}
+	
+	handleMessageSubmit(message) {
+	  const messages = [message, ...this.state.messages];
+	  this.setState({messages});
+	  socket.emit('message', message);
+	}
+
+	handleUserSubmit(name) {
+	  this.setState({name});
+	  socket.emit('join', name);
+	}
+
 	render() {
 		return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
 	}
@@ -49,30 +78,6 @@ class App extends Component {
 	      </div>
 	   );
 	}
-
-	renderUserForm() {
-	   return (<UserForm onUserSubmit={name => this.handleUserSubmit(name)} />)
-	}
-
-	componentDidMount() {
-	  socket.on('message', message => this.messageReceive(message));
-	  socket.on('update', ({users}) => this.chatUpdate(users));
-	}
-
-	messageReceive(message) {
-	  const messages = [message, ...this.state.messages];
-	  this.setState({messages});
-	}
-
-	chatUpdate(users) {
-	  this.setState({users});
-	}
-	
-	handleMessageSubmit(message) {
-	  const messages = [message, ...this.state.messages];
-	  this.setState({messages});
-	  socket.emit('message', message);
-	}	
 }
 
 export default hot(module)(App);
--- index.js
+++ index.js
@@ -1,44 +1,50 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const UsersService = require('./UsersService');
-
+const UsersService = require("./UsersService");
 const usersService = new UsersService();
 
 app.use(express.static(`${__dirname}/public`));
-app.get('/', (req, res) => {
-	res.sendFile(`${__dirname}/index.html`);
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
 });
 
-io.on('connection', (socket) => {
-	socket.on('join', (name) => {
-		usersService.addUser({
-			id: socket.id,
-			name
-		});
-		io.emit('update', {
-			users: usersService.getAllUsers()
-		});
-	});
-	socket.on('disconnect', (socket) => {
-		usersService.removeUser(socket.id);
-		socket.brodcast.emit('update', {
-			users: usersService.getAllUsers()
-		});
-	});
-	socket.on('message', (message) => {
-		const {name} = usersService.getUserById(socket.id);
-		socket.brodcast.emit('message', {
-			text: message.text,
-			from: name
-		});
-	});
+io.on("connection", socket => {
+  socket.on("join", name => {
+    usersService.addUser({
+      id: socket.id,
+      name
+    });
+    io.emit("update", {
+      users: usersService.getAllUsers()
+    });
+  });
+
+  io.on("connection", socket => {
+    socket.on("disconnect", () => {
+      usersService.removeUser(socket.id);
+      socket.broadcast.emit("update", {
+        users: usersService.getAllUsers()
+      });
+    });
+  });
+
+  io.on("connection", socket => {
+    socket.on("message", message => {
+      const { name } = usersService.getUserById(socket.id);
+      socket.broadcast.emit("message", {
+        text: message.text,
+        from: name
+      });
+    });
+  });
 });
 
 server.listen(3000, () => {
-	console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
